5c8154fe8bd9c3f5a441551ccd1adc10d5642a5c
Peter Verhas <peter@verhas.com>
2014-02-18 20:56:48 +0100

    maven plugin development

---
+++ src/main/java/com/javax0/socodot/maven/Compile.java
@@ -0,0 +1,35 @@
+package com.javax0.socodot.maven;
+
+import java.io.File;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugin.logging.Log;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import com.javax0.socodot.Socodot;
+
+@Mojo(name = "compile")
+public class Compile extends AbstractMojo {
+
+	@Parameter(alias = "configurationFile", defaultValue = "")
+	private String configurationFile;
+
+	final Log LOG = getLog();
+
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		LOG.info("Executing socodot compile using configuration '"
+				+ configurationFile + "'");
+		final Socodot engine;
+		if (configurationFile == null || configurationFile.length() == 0) {
+			engine = new Socodot();
+		} else {
+			engine = new Socodot(new File(configurationFile));
+		}
+		engine.execute();
+	}
+
+}
